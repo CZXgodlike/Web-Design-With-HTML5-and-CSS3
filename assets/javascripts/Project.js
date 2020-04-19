018b1dc84827f2e8eadd66dba2ac79599ea7fdf5
CZXgodlike <997232782@qq.com>
2020-04-18 22:51:59 -0700

    Apr 18

---
+++ assets/javascripts/Project.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  var x = document.getElementById("mySidebar");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
